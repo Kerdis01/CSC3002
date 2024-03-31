c56b1c9b5e6afad8eb9580873c8f5d13814d6e8f
Kerdis01 <kyle.erdis@gmail.com>
2024-03-31 20:51:16 +0100

    added upload to s3 function, some optimisation. Needs more tweaking but park for now.

--- yolov8/detectv3.py
+++ yolov8/detectv3.py
@@ -2,54 +2,65 @@ import csv
 import subprocess
 import time
 import os
-import torch
 from ultralytics import YOLO
+from datetime import datetime
+from upload_to_s3 import upload_to_s3
 import resource_monitor
 
+# Model Settings 
+# model_name = 'best-t5.pt' # Model with forward pass and new weights from backpropagation
+model_name = 'yolov8n.pt'
+
+# CSV and AWS Settings
+run_title = datetime.now().strftime('Run_%H%M%S_%d%m%Y_')+model_name
+s3_bucket_name = 'detect-csv-results'  
 results_dir = 'results'
 os.makedirs(results_dir, exist_ok=True)
-csv_file_path = os.path.join(results_dir, 'detection_metrics.csv')
+csv_file_path = os.path.join(results_dir, f'{run_title}.csv')
 
-# Command to launch the camera and create a TCP stream
+# Camera Settings
+stream_url = 'tcp://127.0.0.1:8888'
 camera_command = [
     'libcamera-vid',
     '-n',
     '-t', '0',
     '--width', '800',
     '--height', '640',
-    '--framerate', '1',
+    '--framerate', '5',
     '--inline',
     '--listen',
-    '-o', 'tcp://127.0.0.1:8888'
+    '-o', stream_url
 ]
 
-# Launch the camera in a subprocess
+# Launch Camera
 subprocess.Popen(camera_command)
 time.sleep(5)  # Wait for the camera to be ready
+
+# Load the YOLOv8 model
+model = YOLO(model_name)
+# model.half()
+model.fuse()
+# model.eval()  # Apply quantization
+# metrics = model.val(data='coco8.yaml',
+                            #    imgsz=640,
+                            #    batch=16,
+                            #    conf=0.25,
+                            #    iou=0.6,
+                            #    device='cpu')
+# precision = metrics.box.maps
 
 # Prepare CSV file for logging
 with open(csv_file_path, 'w', newline='') as csvfile:
     csv_writer = csv.writer(csvfile)
     # Write CSV header
-    csv_writer.writerow(["Frame", "CPU Usage (%)", "CPU Temperature (C)", "Core Voltage (V)", "Detection Summary", "Detection Pre-process Speed (ms)", "Detection Inference Speed (ms)", "Detection Post Process Speed (ms)"])
-
-    # Load the YOLOv8 model
-    model = YOLO('yolov8n.pt')
-    model.fuse()
-    model.eval()
-    quantized_model = torch.quantization.quantize_dynamic(
-        model,  # the original model
-        {torch.nn.Conv2d, torch.nn.Linear},  # specify which layers to quantize
-        dtype=torch.qint8  # specify the quantization datatype
-    )
-
-    stream_url = 'tcp://127.0.0.1:8888'
-
+    csv_writer.writerow(["Frame", "CPU Usage (%)", "CPU Temperature (C)", "Core Voltage (V)", "Detection Summary",
+                        "Detection Pre-process Speed (ms)", "Detection Inference Speed (ms)", "Detection Post Process Speed (ms)", "Precision", "Recall", "F1 Score"])
     # Use source=0 for a USB webcam
-    results = quantized_model.track(source=stream_url, stream=True, conf=0.5, tracker="bytetrack.yaml")
+    results = model.track(source=stream_url, stream=True,
+                          conf=0.5, iou=0.7, tracker="bytetrack.yaml", show=True)
     frame_counter = 0  # Initialize frame counter
 
-    #IMPORTANT: ADD ACCURACY
+    # IMPORTANT: ADD ACCURACY
     for result in results:
         frame_counter += 1
 
@@ -65,14 +76,16 @@ with open(csv_file_path, 'w', newline='') as csvfile:
         postprocess_speed = result.speed['postprocess']
 
         # Write data to CSV
-        csv_writer.writerow([frame_counter, cpu_usage, cpu_temp, core_voltage, detection_summary, preprocess_speed, inference_speed, postprocess_speed])
+        csv_writer.writerow([frame_counter, cpu_usage, cpu_temp, core_voltage,
+                            detection_summary, preprocess_speed, inference_speed, postprocess_speed])
 
         # Print the frame and detection summary to the console
         print(f"Frame {frame_counter}: {detection_summary}")
 
         # Break the loop after processing 10 frames
-        if frame_counter == 10:
+        if frame_counter > 10:
             break
 
+upload_to_s3(s3_bucket_name, csv_file_path)
 # Clean up
 subprocess.call(['pkill', '-f', 'libcamera-vid'])
---
+++ yolov8/upload_to_s3.py
@@ -0,0 +1,20 @@
+from credentials import AWS_ACCESS_KEY_ID, AWS_SECRET_ACCESS_KEY
+import os
+import boto3
+
+def upload_to_s3(bucket_name, file_path, object_name=None):
+    if object_name is None:
+        object_name = os.path.basename(file_path)
+
+    s3_client = boto3.client(
+        's3',
+        aws_access_key_id=AWS_ACCESS_KEY_ID,
+        aws_secret_access_key=AWS_SECRET_ACCESS_KEY
+    )
+    try:
+        s3_client.upload_file(file_path, bucket_name, object_name)
+        print(f"File {file_path} uploaded to {bucket_name}/{object_name}")
+        os.remove(file_path)
+    except Exception as e:
+        print(f"Failed to upload {file_path} to {bucket_name}/{object_name}.")
+        print(e)
